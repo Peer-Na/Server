454f39fdf0feebacd2682d1b88dccfe7a6f9e998
sseunghoon <sseunghun99@naver.com>
2024-04-02 14:28:01 +0900

    refac: PeernaApplication 삭제 및 분리

---
+++ domain/integrated/src/main/java/cos/peerna/PeernaDomainRoot.java
@@ -0,0 +1,7 @@
+package cos.peerna;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackageClasses = {PeernaDomainRoot.class})
+public interface PeernaDomainRoot {
+}
