fb3e87e9b930b071f5d78f08ddc423fbcb22a135
sseunghoon <sseunghun99@naver.com>
2024-04-07 17:52:45 +0900

    feat: histories API 문서화 및 스펙 변경

--- app/external-api/src/main/java/cos/peerna/controller/history/HistoryController.java
+++ app/external-api/src/main/java/cos/peerna/controller/history/HistoryController.java
@@ -1,12 +1,18 @@
 package cos.peerna.controller.history;
 
+import cos.peerna.controller.dto.ResponseDto;
 import cos.peerna.controller.history.response.DetailHistoryResponse;
 import cos.peerna.controller.history.response.HistoryListResponse;
+import cos.peerna.controller.history.response.HistoryResponse;
+import cos.peerna.controller.reply.response.ReplyResponse;
 import cos.peerna.domain.history.dto.result.DetailHistoryResult;
+import cos.peerna.domain.history.dto.result.HistoryResult;
 import cos.peerna.domain.history.service.HistoryService;
+import cos.peerna.domain.user.model.Category;
 import cos.peerna.global.security.LoginUser;
 import cos.peerna.global.security.dto.SessionUser;
 import jakarta.annotation.Nullable;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,32 +23,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/history")
+@RequestMapping("/api/histories")
 public class HistoryController {
     private final HistoryService historyService;
     @GetMapping
-    public ResponseEntity<HistoryListResponse> findUserHistory(
+    public ResponseEntity<ResponseDto<HistoryListResponse>> findUserHistory(
             @RequestParam Long userId,
             @RequestParam(required = false, defaultValue = "0") Long cursorId,
             @RequestParam(required = false, defaultValue = "10") int size) {
-        return ResponseEntity.ok(HistoryListResponse.builder()
-                .historyList(historyService.findUserHistory(userId, cursorId, size))
-                .build());
+
+        List<HistoryResult> historyList = historyService.findUserHistory(userId, cursorId, size);
+        return ResponseEntity.ok(ResponseDto.of(
+                        HistoryListResponse.builder()
+                                .histories(historyList.stream()
+                                        .map(HistoryResponse::of).toList()).build()));
     }
 
     @GetMapping("/detail")
-    public ResponseEntity<DetailHistoryResponse> findDetailHistory(
+    public ResponseEntity<ResponseDto<DetailHistoryResponse>> findDetailHistory(
             @Nullable @LoginUser SessionUser user,
             @RequestParam Long historyId) {
 
         DetailHistoryResult result = historyService.findDetailHistory(historyId, user == null ? null : user.getId());
-        return ResponseEntity.ok(DetailHistoryResponse.builder()
-                .question(result.getQuestion())
-                .exampleAnswer(result.getExampleAnswer())
-                .time(result.getTime())
-                .replies(result.getReplies())
+        return ResponseEntity.ok(ResponseDto.of(DetailHistoryResponse.builder()
+                .date(result.getTime())
+                .replies(result.getReplies().stream()
+                        .map(ReplyResponse::of).toList())
                 .keywords(result.getKeywords())
                 .chattings(result.getChattings())
-                .build());
+                .build()));
     }
 }
