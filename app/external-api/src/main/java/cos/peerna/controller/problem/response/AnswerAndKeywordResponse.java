075d2f3d13a193c043631968f5c1fea00c7ca115
sseunghoon <sseunghun99@naver.com>
2024-04-07 16:18:26 +0900

    feat: problems API 문서화 및 스펙 변경

--- app/external-api/src/main/java/cos/peerna/controller/problem/response/AnswerAndKeywordResponse.java
+++ app/external-api/src/main/java/cos/peerna/controller/problem/response/AnswerAndKeywordResponse.java
@@ -2,6 +2,7 @@ package cos.peerna.controller.problem.response;
 
 import cos.peerna.domain.problem.dto.result.AnswerAndKeywordResult;
 import cos.peerna.domain.problem.dto.result.KeywordResult;
+import java.util.List;
 import lombok.Builder;
 import org.springframework.validation.annotation.Validated;
 
@@ -9,7 +10,7 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public record AnswerAndKeywordResponse(
 		String answer,
-		KeywordResult keywords
+		List<String> keywords
 ) {
 
 	public static AnswerAndKeywordResponse of(AnswerAndKeywordResult result) {
